44da845aa2e387c9cba5e077466936a5f43348fb
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2024-07-10 16:56:39 +0200

    Ajout du ProductController et modification des routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+route::get ('/products',function (){
+    return 'Hello World';
+});
+
+    route::get ('/products/{id}',function ($id){
+        return 'Hello '.$id;
+    });
