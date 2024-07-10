09571f69db86c1ce28612f260b74d7a1da5d4cf5
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-10 16:29:17 +0200

    added categories routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categorie', function () {
+    return 'Liste des catégories';
+});
+
+Route::get('/categorie/{id}', function () {
+    return 'Page catégorie';
+});
