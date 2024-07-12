89a4e2d23ddbddfe3c2f6642b50252116156f824
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-12 10:07:42 +0200

    added routes and controllers with teacher agreement

--- routes/web.php
+++ routes/web.php
@@ -21,28 +21,12 @@ Route::prefix('/categorie')->group(function () {
 route::prefix('/Produits')->group(function () {
     Route::get('/', [\App\Http\Controllers\ProductController::class, 'index']);
 
-/*
-get => pour informer qu'on repond à la methode "get" du navigateur.
-
-"/" => URL en premier parametre "/" = root
-function\() => permettra d'explique comment repondre
-return view('welcome'); => lorsque j'accede a la page racine, il faut que tu reponde en retournant une view qui s'appelle "welcome".
-
-*/
-
     Route::get('/user', [UserController::class, 'index']);
-
-Route::get('/categorie', function () {
-    return 'Liste des catégories';
-});
 
 Route::prefix('/categorie')->group(function () {
     Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
 
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
-
-    Route::resource('carts', CartController::class);
-
 });
 
     Route::resource('users', UserController::class);
