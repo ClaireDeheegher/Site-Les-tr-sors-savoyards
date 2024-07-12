a0e3d4c7c8b8a7863a904969c2e7fde476e4e018
Cheick  Diabate <cheick.diabate@gmail.com>
2024-07-12 16:56:28 +0200

    vendredi1207

---
+++ app/Http/Controllers/GererController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GererController extends Controller
+{
+    public function index() {
+        /*$test = [
+            "Article" => 'Article5',
+        ];*/
+        return "hello world";
+
+    }
+}
