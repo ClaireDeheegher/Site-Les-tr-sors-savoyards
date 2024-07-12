9d7db281158df2d92a99b4ac08a62608cf7ee906
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-12 10:07:42 +0200

    added routes and controllers with teacher agreement

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,7 @@ function\() => permettra d'explique comment repondre
 return view('welcome'); => lorsque j'accede a la page racine, il faut que tu reponde en retournant une view qui s'appelle "welcome".
 
 */
-
     Route::get('/user', [UserController::class, 'index']);
-
-Route::get('/categorie', function () {
-    return 'Liste des catégories';
-});
 
 Route::prefix('/categorie')->group(function () {
 
@@ -34,7 +29,7 @@ Route::prefix('/categorie')->group(function () {
     Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
 
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
-
-    Route::resource('carts', CartController::class);
-
 });
+
+Route::resource('carts', CartController::class);
+
