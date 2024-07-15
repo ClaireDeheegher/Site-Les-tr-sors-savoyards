72cc8538a7b057b4fe7913ec10928f79902c0b4d
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-15 11:38:51 +0200

    added model with modified routes and controllers for categorie

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::get('/', function () {
 });
 
 Route::prefix('/categorie')->group(function () {
+
+
     Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
 
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
