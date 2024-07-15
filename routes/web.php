a835d43e8cc6caf62c00f1d82da95c42913a072d
poetta <pedro.costa@le-campus-numerique.fr>
2024-07-15 16:44:06 +0200

    +1

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,8 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
 return view('welcome');
 });
+
 Route::prefix('/categorie')->group(function () {
-
-
-    Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
-
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
 
 });
