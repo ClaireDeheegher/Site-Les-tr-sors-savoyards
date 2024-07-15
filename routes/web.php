76aec59aa844c1de24a41465d7b0a0bb50dc1115
poetta <pedro.costa@le-campus-numerique.fr>
2024-07-15 16:44:06 +0200

    +1

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*ROUTE TEST
-Route => on utilise une class definit dans ce namespace: "use Illuminate\Support\Facades\Route;"
-
-get => pour informer qu'on repond à la methode "get" du navigateur.
-
-"/" => URL en premier parametre "/" = root
-function\() => permettra d'explique comment repondre
-return view('welcome'); => lorsque j'accede a la page racine, il faut que tu reponde en retournant une view qui s'appelle "welcome".
-
-*/
-    Route::get('/user', [UserController::class, 'index']);
 
 Route::prefix('/categorie')->group(function () {
-
-
-    Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
-
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
 });
 
-Route::resource('carts', CartController::class);
+    Route::resource('carts', CartController::class);
 
+    Route::resource('users', UserController::class);
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
