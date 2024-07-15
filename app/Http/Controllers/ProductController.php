e207b4bf1bd83ecccc022def513ea2286f54841e
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2024-07-15 10:22:45 +0200

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\JsonResponse;
 
-use Illuminate\Http\Request;
 
-class ProductController extends Controller
-{
 
+class ProductController extends Controller{
+public function index(): JsonResponse{
+    $test = [
+        'name' => 'Abigail',
+        'state' => 'CA',
+    ];
+
+    return response()->json($test);
 }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
-
-
-    use App\Http\Controllers\CartController;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });
 
 Route::prefix('/categorie')->group(function () {
@@ -15,10 +12,5 @@ Route::prefix('/categorie')->group(function () {
     Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
 
     Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
-
-
-    Route::resource('carts', CartController::class);
-
-
 
 });
