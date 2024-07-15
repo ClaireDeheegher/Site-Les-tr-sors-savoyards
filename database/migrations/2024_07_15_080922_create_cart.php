d9ce252d2f866a748dae38f34acffd63888bf744
Cheick  Diabate <cheick.diabate@gmail.com>
2024-07-15 15:58:43 +0200

    update15apresmidi

--- database/migrations/2024_07_15_080922_create_cart.php
+++ database/migrations/2024_07_15_080922_create_cart.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
-            $table->integer('quantity');
+            $table->integer('quantity'); // Assurez-vous que cette colonne est de type entier
+            $table->timestamps();
         });
     }
 
