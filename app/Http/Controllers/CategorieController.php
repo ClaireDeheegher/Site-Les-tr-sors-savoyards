733ebdd74205daef395f986edf377aaad7ac4b94
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-15 14:20:49 +0200

    modified controller and model to make it work #Debugging

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -2,19 +2,24 @@
 
 namespace App\Http\Controllers;
 
-class CategorieController extends Controller
+class CategorieController extends Controller //Classe qui va servir de controller pour afficher les pages
 {
     public function index()
     {
-        $categorie = new \App\Models\Categorie;
-        $categorie->name = 'Fromage';
-        $categorie->save();
+        $categorie = new \App\Models\Categorie(); // Appel du modèle et de la db
+
+        /* $categorie->name = 'Epicerie'; # Création d'une nouvelle ligne dans la db avec le nom de la catégorie
+         $categorie->save();*/
         return \App\Models\Categorie::all();
     }
 
     public function showCategorie(string $id)
     {
-        $categorie = new \App\Models\Categorie;
-        return \App\Models\Categorie::all('name');
+        $categorie = \App\Models\Categorie::all();
+
+        dd($categorie[$id]->name);
+
+        return $categorie;
+
     }
 }
