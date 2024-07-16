330877be62b095c82f6cc2d763e88f1d9ce859af
Cheick  Diabate <cheick.diabate@gmail.com>
2024-07-16 10:26:20 +0200

    restore web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,27 +12,19 @@ return view('welcome');
 
     Route::prefix('/categorie')->group(function () {
 
+        Route::get('/', [\App\Http\Controllers\CategorieController::class, 'index']);
+        Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
 
 
-    Route::get('/{id}', [\App\Http\Controllers\CategorieController::class, 'showCategorie']);
-
-});
-
-<<<<<<< HEAD
-
-route::get ('/products',function (){
-    return 'Hello World';
-=======
-route::prefix('/Produits')->group(function () {
-    Route::get('/', [\App\Http\Controllers\ProductController::class, 'index']);
-
-    Route::get('/{id}', [\App\Http\Controllers\ProductController::class, 'ShowProduits']);
->>>>>>> a456567 (+2)
-});
-
-    route::get ('/products/{id}',function ($id){
-        return 'Hello '.$id;
     });
+
+    route::prefix('/Produits')->group(function () {
+        Route::get('/', [\App\Http\Controllers\ProductController::class, 'index']);
+
+        Route::get('/{id}', [\App\Http\Controllers\ProductController::class, 'ShowProduits']);
+    });
+
+
 
     Route::resource('users', UserController::class);
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
