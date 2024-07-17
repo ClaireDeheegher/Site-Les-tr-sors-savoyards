986b1716889a5ee773928fa536abc6337a58b037
poetta <pedro.costa@le-campus-numerique.fr>
2024-07-17 17:00:38 +0200

    projet a jour

---
+++ views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <style>
+        .table td, .table th {
+            vertical-align: middle;
+            text-align: center;
+        }
+    </style>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
