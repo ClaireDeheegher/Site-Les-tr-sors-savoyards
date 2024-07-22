0af1150018f19fdba0c3f367f244e13b812d15b8
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-22 13:55:16 +0200

    added CRUD for order

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,104 +1,107 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\User;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    public function index()
     {
-        public function index()
-        {
-            $users = User::all();
-            return response()->json($users);
-        }
+        $users = User::all();
 
-        public function create()
-        {
-            return view('users.create');
-        }
-
-        public function store(Request $request)
-        {
-
-            $user = new User([
-                'name' => $request->input('name'),
-                'lastname' => $request->input('lastname'),
-                'birthday' => $request->input('birthday'),
-                'gender' => $request->input('gender'),
-                'phone' => $request->input('phone'),
-                'address' => $request->input('address'),
-                'address2' => $request->input('address2'),
-                'zipcode' => $request->input('zipcode'),
-                'town' => $request->input('town'),
-                'country' => $request->input('country'),
-                'email' => $request->input('email'),
-                'password' => Hash::make($request->input('password')),
-            ]);
-
-            $user->save();
-
-            return response()->json(['message' => 'Utilisateur créé avec succès!', 'user' => $user]);
-        }
-
-        public function show($id)
-        {
-            $user = User::findOrFail($id);
-            return response()->json($user);
-        }
-
-        public function edit($id)
-        {
-            $user = User::findOrFail($id);
-            return view('users.edit', compact('user'));
-        }
-
-        public function update(Request $request, $id): \Illuminate\Http\JsonResponse
-        {
-            $request->validate([
-                'name' => "required|string",
-                'lastname' => "required|string",
-                'birthday' => "required|date",
-                'gender' => "required|string",
-                'phone' => "required|string",
-                'address' => "required|string",
-                'address2' => "nullable|string",
-                'zipcode' => "required|string",
-                'town' => "required|string",
-                'country' => "required|string",
-                'email' => "required|string|email",
-                'password' => "nullable|string",
-            ]);
-
-            $user = User::findOrFail($id);
-
-            $user->name = $request->get('name');
-            $user->lastname = $request->get('lastname');
-            $user->birthday = $request->get('birthday');
-            $user->gender = $request->get('gender');
-            $user->phone = $request->get('phone');
-            $user->address = $request->get('address');
-            $user->address2 = $request->get('address2');
-            $user->zipcode = $request->get('zipcode');
-            $user->town = $request->get('town');
-            $user->country = $request->get('country');
-            $user->email = $request->get('email');
-
-            if ($request->filled('password')) {
-                $user->password = Hash::make($request->get('password'));
-            }
-
-            $user->save();
-
-            return response()->json(['message' => 'Utilisateur mis à jour avec succès.']);
-        }
-
-        public function destroy($id)
-        {
-            $user = User::findOrFail($id);
-            $user->delete();
-
-            return response()->json(['message' => 'Utilisateur supprimé avec succès.']);
-        }
+        return response()->json($users);
     }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $user = new User([
+            'name' => $request->input('name'),
+            'lastname' => $request->input('lastname'),
+            'birthday' => $request->input('birthday'),
+            'gender' => $request->input('gender'),
+            'phone' => $request->input('phone'),
+            'address' => $request->input('address'),
+            'address2' => $request->input('address2'),
+            'zipcode' => $request->input('zipcode'),
+            'town' => $request->input('town'),
+            'country' => $request->input('country'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        $user->save();
+
+        return response()->json(['message' => 'Utilisateur créé avec succès!', 'user' => $user]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json($user);
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'lastname' => 'required|string',
+            'birthday' => 'required|date',
+            'gender' => 'required|string',
+            'phone' => 'required|string',
+            'address' => 'required|string',
+            'address2' => 'nullable|string',
+            'zipcode' => 'required|string',
+            'town' => 'required|string',
+            'country' => 'required|string',
+            'email' => 'required|string|email',
+            'password' => 'nullable|string',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->get('name');
+        $user->lastname = $request->get('lastname');
+        $user->birthday = $request->get('birthday');
+        $user->gender = $request->get('gender');
+        $user->phone = $request->get('phone');
+        $user->address = $request->get('address');
+        $user->address2 = $request->get('address2');
+        $user->zipcode = $request->get('zipcode');
+        $user->town = $request->get('town');
+        $user->country = $request->get('country');
+        $user->email = $request->get('email');
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->get('password'));
+        }
+
+        $user->save();
+
+        return response()->json(['message' => 'Utilisateur mis à jour avec succès.']);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'Utilisateur supprimé avec succès.']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,58 +1,69 @@
 <?php
 
-    use App\Http\Controllers\ProductController;
-    use App\Http\Controllers\CartController;
-    use App\Http\Controllers\UserController;
-    use App\Http\Controllers\CategorieController;
-    use Illuminate\Support\Facades\Route;
-    use Illuminate\Http\Request;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-   Route::prefix('carts')->group(function () {
+Route::prefix('carts')->group(function () {
     Route::get('/', [CartController::class, 'index']);
     Route::get('/{id}', [CartController::class, 'show']);
     Route::post('/', [CartController::class, 'store']);
     Route::put('/{id}', [CartController::class, 'update']);
     Route::delete('/{id}', [CartController::class, 'destroy']);
-   });
+});
 
 // Routes pour les utilisateurs
-    Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users.index')->withoutMiddleware('auth:sanctum');
-        Route::get('/create', [UserController::class, 'create'])->name('users.create')->withoutMiddleware('auth:sanctum');
-        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit')->withoutMiddleware('auth:sanctum');
-        Route::put('/{user}', [UserController::class, 'update'])->name('users.update')->withoutMiddleware('auth:sanctum');
-        Route::post('/', [UserController::class, 'store'])->name('users.store')->withoutMiddleware('auth:sanctum');
-        Route::get('/{user}', [UserController::class, 'show'])->name('users.show')->withoutMiddleware('auth:sanctum');
-        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy')->withoutMiddleware('auth:sanctum');
-    });
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index')->withoutMiddleware('auth:sanctum');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create')->withoutMiddleware('auth:sanctum');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit')->withoutMiddleware('auth:sanctum');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update')->withoutMiddleware('auth:sanctum');
+    Route::post('/', [UserController::class, 'store'])->name('users.store')->withoutMiddleware('auth:sanctum');
+    Route::get('/{user}', [UserController::class, 'show'])->name('users.show')->withoutMiddleware('auth:sanctum');
+    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy')->withoutMiddleware('auth:sanctum');
+});
 
 // Routes pour les produits
-    route::prefix('/products')->group(function () {
-        Route::get('/', [ProductController::class, 'index']);
-        Route::get('/{id}', [ProductController::class, 'ShowProduits']);
-        Route::post('/', [ProductController::class, 'products']);
-        Route::put('/{id}', [ProductController::class, 'update']);
-        Route::delete('/{id}', [ProductController::class, 'destroy']);
-    });
+route::prefix('/products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'ShowProduits']);
+    Route::post('/', [ProductController::class, 'products']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
 
 // Routes pour les catégories
-    Route::prefix('/categorie')->group(function () {
-        Route::get('/', [CategorieController::class, 'index']);
-        Route::get('/{id}', [CategorieController::class, 'showCategorie']);
-    });
+Route::prefix('/categorie')->group(function () {
+    Route::get('/', [CategorieController::class, 'index']);
+    Route::get('/{id}', [CategorieController::class, 'showCategorie']);
+});
 
 // Routes pour les paniers
-    Route::resource('carts', CartController::class);
+Route::resource('carts', CartController::class);
+
+// Routes pour les commandes
+Route::prefix('/orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+    Route::post('/', [OrderController::class, 'create']);
+    Route::put('/{id}', [OrderController::class, 'update']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+
+});
 
 // Exemple de route pour obtenir l'utilisateur authentifié
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->withoutMiddleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->withoutMiddleware('auth:sanctum');
