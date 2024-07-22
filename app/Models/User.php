2063bc929ee99594459a562ee843d2fb2a73e71b
Cheick  Diabate <cheick.diabate@gmail.com>
2024-07-22 11:34:30 +0200

    change conflict

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,10 @@
 <?php
 
     namespace App\Models;
-<<<<<<< HEAD
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-
-=======
 
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
 
->>>>>>> a456567 (+2)
     class User extends Model
     {
         use HasFactory;
