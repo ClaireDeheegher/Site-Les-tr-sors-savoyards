826d428bc306e9e1b657637e12e42bec2a474a7a
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-22 11:57:36 +0200

    deleting doublons on migrate file personal token and changing order migration file to remove indexname

--- database/migrations/2024_07_22_075205_orders.php
+++ database/migrations/2024_07_22_075205_orders.php
@@ -14,17 +14,17 @@ return new class extends Migration
         Schema::disableForeignKeyConstraints();
         Schema::create('orders', static function (Blueprint $table) {
             $table->id();
-            $table->foreignId('lastname_id')->constrained('users', indexName: 'id');
-            $table->foreignId('name_id')->constrained('users', indexName: 'id');
-            $table->foreignId('address_id')->constrained('users', indexName: 'id');
-            $table->foreignId('address2_id')->constrained('users', indexName: 'id')->nullable;
-            $table->foreignId('zipcode_id')->constrained('users', indexName: 'id');
-            $table->foreignId('town_id')->constrained('users', indexName: 'id');
-            $table->foreignId('email_id')->constrained('users', indexName: 'id');
-            $table->foreignId('phone_id')->constrained('users', indexName: 'id');
-            $table->foreignId('productname_id')->constrained('p_cart', indexName: 'id');
-            $table->foreignId('productprice_id')->constrained('p_cart', indexName: 'id');
-            $table->foreignId('productquantity_id')->constrained('p_cart', indexName: 'id');
+            $table->foreignId('lastname_id')->constrained('users');
+            $table->foreignId('name_id')->constrained('users');
+            $table->foreignId('address_id')->constrained('users');
+            $table->foreignId('address2_id')->constrained('users')->nullable;
+            $table->foreignId('zipcode_id')->constrained('users');
+            $table->foreignId('town_id')->constrained('users');
+            $table->foreignId('email_id')->constrained('users');
+            $table->foreignId('phone_id')->constrained('users');
+            $table->foreignId('productname_id')->constrained('p_cart');
+            $table->foreignId('productprice_id')->constrained('p_cart');
+            $table->foreignId('productquantity_id')->constrained('p_cart');
         });
     }
 
