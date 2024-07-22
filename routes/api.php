e91ce2dbe10ec34ccf9faf52e40f3df5b52e7db3
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-22 09:50:37 +0200

    modify route

--- routes/api.php
+++ routes/api.php
@@ -1,46 +1,46 @@
 <?php
 
-    use App\Http\Controllers\ProductController;
-    use App\Http\Controllers\CartController;
-    use App\Http\Controllers\UserController;
-    use App\Http\Controllers\CategorieController;
-    use Illuminate\Support\Facades\Route;
-    use Illuminate\Http\Request;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Routes pour les utilisateurs
-    Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users.index')->withoutMiddleware('auth:sanctum');
-        Route::get('/create', [UserController::class, 'create'])->name('users.create')->withoutMiddleware('auth:sanctum');
-        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit')->withoutMiddleware('auth:sanctum');
-        Route::put('/{user}', [UserController::class, 'update'])->name('users.update')->withoutMiddleware('auth:sanctum');
-        Route::post('/', [UserController::class, 'store'])->name('users.store')->withoutMiddleware('auth:sanctum');
-        Route::get('/{user}', [UserController::class, 'show'])->name('users.show')->withoutMiddleware('auth:sanctum');
-        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy')->withoutMiddleware('auth:sanctum');
-    });
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index')->withoutMiddleware('auth:sanctum');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create')->withoutMiddleware('auth:sanctum');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit')->withoutMiddleware('auth:sanctum');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update')->withoutMiddleware('auth:sanctum');
+    Route::post('/', [UserController::class, 'store'])->name('users.store')->withoutMiddleware('auth:sanctum');
+    Route::get('/{user}', [UserController::class, 'show'])->name('users.show')->withoutMiddleware('auth:sanctum');
+    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy')->withoutMiddleware('auth:sanctum');
+});
 
 // Routes pour les produits
-    route::prefix('/produits')->group(function () {
-        Route::get('/', [ProductController::class, 'index']);
-        Route::get('/{id}', [ProductController::class, 'ShowProduits']);
-        Route::post('/', [ProductController::class, 'products']);
-        Route::put('/{id}', [ProductController::class, 'update']);
-        Route::delete('/{id}', [ProductController::class, 'destroy']);
-    });
+route::prefix('/produits')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'ShowProduits']);
+    Route::post('/', [ProductController::class, 'products']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
 
 // Routes pour les catégories
-    Route::prefix('/categorie')->group(function () {
-        Route::get('/', [CategorieController::class, 'index']);
-        Route::get('/{id}', [CategorieController::class, 'showCategorie']);
-    });
+Route::prefix('/categorie')->group(function () {
+    Route::get('/', [CategorieController::class, 'index']);
+    Route::get('/{id}', [CategorieController::class, 'showCategorie']);
+});
 
 // Routes pour les paniers
-    Route::resource('carts', CartController::class);
+Route::resource('carts', CartController::class);
 
 // Exemple de route pour obtenir l'utilisateur authentifié
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->withoutMiddleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->withoutMiddleware('auth:sanctum');
