31a3f83b15709ef352305e410f9549472ef9db05
Cheick  Diabate <cheick.diabate@gmail.com>
2024-07-22 13:55:52 +0200

    modelsPcart

--- app/Models/Produits.php
+++ app/Models/Produits.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Produits extends Model
 {
@@ -16,7 +17,11 @@ class Produits extends Model
     public function categorie()
     {
         return $this->belongsTo(categorie::class);
-    {
     }
-}
+
+    public function pCart() : HasMany{
+        return $this->hasMany(PCart::class);
+    }
+
+
 }
