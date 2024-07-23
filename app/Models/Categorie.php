a7d002ceb897dab6cdf6bb8304a934d030879fe1
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-23 15:01:01 +0200

    typo mistake while rebasing

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Builder;
 
 class Categorie extends Model
 {
@@ -15,9 +14,7 @@ class Categorie extends Model
 
     protected $fillable = ['name'];
 
-    public static function find(string $id)
-    {
-    }
+    public static function find(string $id) {}
 
     public function produits(): HasMany
     {
