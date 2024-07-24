ee18290f242c75d7aa654e0e8a0cfd3d159bd44f
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-24 11:18:40 +0200

    typo mistake

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Hash;
+    use function Laravel\Prompts\password;
 
-class UserController extends Controller
-{
-    public function index()
+    class UserController extends Controller
     {
-        $users = User::all();
+        public function index()
+        {
+            $users = User::all();
 
-        return response()->json($users);
-        //return view('users.index', compact('users'));
-    }
+            return response()->json($users);
+            //return view('users.index', compact('users'));
+        }
 
         public function create()
         {
@@ -54,72 +55,72 @@ class UserController extends Controller
             'password' => Hash::make($request->input('password')),
         ]);
 
-            $user->save();
+        $user->save();
 
-            return response()->json(['message' => 'Utilisateur créé avec succès!', 'user' => $user]);
-        }
-
-        public function show($id)
-        {
-            $user = User::findOrFail($id);
-
-            return response()->json($user);
-            //return view('users.show', compact('user'));
-        }
-
-        public function edit($id)
-        {
-            $user = User::findOrFail($id);
-
-            return view('users.edit', compact('user'));
-        }
-
-        public function update(Request $request, $id): \Illuminate\Http\JsonResponse
-        {
-            // Validation
-            $request->validate([
-                'name' => 'required|string|max:255',
-                'lastname' => 'required|string|max:255',
-                'birthday' => 'required|date',
-                'gender' => 'required|string|max:1',
-                'phone' => 'required|string|max:20',
-                'address' => 'required|string|max:255',
-                'address2' => 'nullable|string|max:255',
-                'zipcode' => 'required|string|max:10',
-                'town' => 'required|string|max:255',
-                'country' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users,email,' . $id,
-                'password' => 'nullable|string|min:8|confirmed',
-            ]);
-
-            $user = User::findOrFail($id);
-
-            $user->name = $request->get('name');
-            $user->lastname = $request->get('lastname');
-            $user->birthday = $request->get('birthday');
-            $user->gender = $request->get('gender');
-            $user->phone = $request->get('phone');
-            $user->address = $request->get('address');
-            $user->address2 = $request->get('address2');
-            $user->zipcode = $request->get('zipcode');
-            $user->town = $request->get('town');
-            $user->country = $request->get('country');
-            $user->email = $request->get('email');
-
-            if ($request->filled('password')) {
-                $user->password = Hash::make($request->get('password'));
-            }
-
-            $user->save();
-
-            return response()->json(['message' => 'Utilisateur mis à jour avec succès.']);
-        }
-
-        public function destroy($id)
-        {
-            $user = User::findOrFail($id);
-            $user->delete();
-
-            return response()->json(['message' => 'Utilisateur supprimé avec succès.']);
-        }
+        return response()->json(['message' => 'Utilisateur créé avec succès!', 'user' => $user]);
     }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json($user);
+        //return view('users.show', compact('user'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        // Validation
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'birthday' => 'required|date',
+            'gender' => 'required|string|max:1',
+            'phone' => 'required|string|max:20',
+            'address' => 'required|string|max:255',
+            'address2' => 'nullable|string|max:255',
+            'zipcode' => 'required|string|max:10',
+            'town' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.$id,
+            'password' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->get('name');
+        $user->lastname = $request->get('lastname');
+        $user->birthday = $request->get('birthday');
+        $user->gender = $request->get('gender');
+        $user->phone = $request->get('phone');
+        $user->address = $request->get('address');
+        $user->address2 = $request->get('address2');
+        $user->zipcode = $request->get('zipcode');
+        $user->town = $request->get('town');
+        $user->country = $request->get('country');
+        $user->email = $request->get('email');
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->get('password'));
+        }
+
+        $user->save();
+
+        return response()->json(['message' => 'Utilisateur mis à jour avec succès.']);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'Utilisateur supprimé avec succès.']);
+    }
+}
