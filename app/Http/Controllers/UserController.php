8772675c77995cb13eecb251b0099c9da8cb3331
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-24 11:58:35 +0200

    changes on auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,21 +1,20 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\User;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
-    use function Laravel\Prompts\password;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    public function index()
     {
-        public function index()
-        {
-            $users = User::all();
+        $users = User::all();
 
-            return response()->json($users);
-            //return view('users.index', compact('users'));
-        }
+        return response()->json($users);
+        //return view('users.index', compact('users'));
+    }
 
     public function create()
     {
