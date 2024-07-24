9c62090bae7971c6cd1b3cc9a20492887bd0e75d
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-24 11:18:40 +0200

    typo mistake

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Hash;
+    use function Laravel\Prompts\password;
 
-class UserController extends Controller
-{
-    public function index()
+    class UserController extends Controller
     {
-        $users = User::all();
+        public function index()
+        {
+            $users = User::all();
 
-        return response()->json($users);
-        //return view('users.index', compact('users'));
-    }
+            return response()->json($users);
+            //return view('users.index', compact('users'));
+        }
 
         public function create()
         {
