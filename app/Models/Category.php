b4b533f1d4593b05b934f7234fc98fb9802892d2
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-25 16:46:26 +0200

    update view working

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Builder;
 
 class Category extends Model
 {
@@ -15,9 +16,7 @@ class Category extends Model
 
     protected $fillable = ['name'];
 
-    public static function find(string $id)
-    {
-    }
+    public static function find(string $id) {}
 
     public static function findOrFail($id) {}
 
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,13 +4,17 @@
 @section ('content')
     <h1>Catégories</h1>
     <section>
-        <a href="{{route('category.store')}}" class="btn btn-info btn-sm">Ajouter une catégorie</a>
+        <a href="{{route('category.create')}}" class="btn btn-info btn-sm">Ajouter une catégorie</a>
+
+
     </section>
     <section>
     @foreach($category as $categories)
 
         <h2>{{$categories->name}} </h2>
         <a href="{{route('category.show', $categories->id )}}" class="btn btn-info btn-sm">Voir les produits</a>
+            <a href="{{route('category.edit', $categories->id )}}" class="btn btn-info btn-sm">Modifier la catégorie</a>
+
     @endforeach
     </section>
 
