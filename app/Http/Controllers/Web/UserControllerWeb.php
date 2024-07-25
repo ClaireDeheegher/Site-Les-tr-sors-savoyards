277de76f099825466863ad001d1a7fa7ad26454b
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-25 16:46:26 +0200

    update view working

--- app/Http/Controllers/Web/UserControllerWeb.php
+++ app/Http/Controllers/Web/UserControllerWeb.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
+class UserControllerWeb extends Controller
 {
     public function index()
     {
