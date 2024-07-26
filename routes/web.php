f1adf8a04d72b67e2ac49aab7b27250c7f33849b
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-07-26 11:06:55 +0200

    typo changes before merging

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,15 @@
 // Routes pour le login
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CartController;
-use App\Http\Controllers\View\CategoryController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\View\CategoryController;
 use App\Http\Controllers\View\ProductController;
 use App\Http\Controllers\View\UserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Api\CategoryController;
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Routes pour les carts
 Route::prefix('carts')->group(function () {
@@ -25,48 +23,47 @@ Route::prefix('carts')->group(function () {
 });
 
 // Routes pour les users
-    Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users.index');
-        Route::get('/create', [UserController::class, 'create'])->name('users.create');
-        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::post('/', [UserController::class, 'store'])->name('users.store');
-        Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
-        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-    });
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::post('/', [UserController::class, 'store'])->name('users.store');
+    Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
-    //->Middleware('auth:sanctum')
-
+//->Middleware('auth:sanctum')
 
 // Routes pour les products
-    route::prefix('/products')->group(function () {
-        Route::get('/', [ProductController::class, 'index']);
-        Route::get('/{id}', [ProductController::class, 'ShowProduits']);
-        Route::post('/', [ProductController::class, 'products']);
-        Route::put('/{id}', [ProductController::class, 'update']);
-        Route::delete('/{id}', [ProductController::class, 'destroy']);
-    });
+route::prefix('/products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'ShowProduits']);
+    Route::post('/', [ProductController::class, 'products']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
 
 // Routes pour les catégories
-    Route::prefix('/categorie')->group(function () {
-        Route::get('/', [CategorieController::class, 'index']);
-        Route::get('/{id}', [CategorieController::class, 'showCategorie']);
-        Route::post('/', [CategorieController::class, 'create']);
-        Route::put('/{id}', [CategorieController::class, 'update']);
-        Route::delete('/{id}', [CategorieController::class, 'delete']);
-    });
+Route::prefix('/category')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{id}', [CategoryController::class, 'showCategory']);
+    Route::post('/', [CategoryController::class, 'create']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'delete']);
+});
 
 // Routes pour les paniers
-    Route::resource('carts', CartController::class);
+Route::resource('carts', CartController::class);
 
 // Routes pour les commandes
-    Route::prefix('/orders')->group(function () {
-        Route::get('/', [OrderController::class, 'index']);
-        Route::get('/{id}', [OrderController::class, 'show']);
-        Route::post('/', [OrderController::class, 'create']);
-        Route::put('/{id}', [OrderController::class, 'update']);
-        Route::delete('/{id}', [OrderController::class, 'destroy']);
-    });
+Route::prefix('/orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+    Route::post('/', [OrderController::class, 'create']);
+    Route::put('/{id}', [OrderController::class, 'update']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+});
 
 Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/login', [AuthController::class, 'doLogin']);
